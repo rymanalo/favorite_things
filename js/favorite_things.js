a7af8bc121e89d20d7d48ad6e4b19a83c92ef56d
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-04-23 21:47:03 -0700

    Listing favorite fast food places.

---
+++ js/favorite_things.js
@@ -0,0 +1,12 @@
+var fave_things = [
+  "In-n-Out",
+  "McDonald's",
+  "Jack-in-the-Box",
+  "Kentucky Fried Chicken",
+  "Taco Bell"
+];
+
+function fave_fast_food(array) {
+  for(var i = 0; i < array.length; i += 1)
+    console.log("My #" + (i+1) + " favorite fast food place is " + array[i]);
+};
